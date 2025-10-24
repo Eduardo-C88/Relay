a4308c2c05a8c85bfc7b8b48563a5b6c30c74ba5
Eduardo-C88 <eduardo.cristelo@gmail.com>
2025-10-24 15:40:35 +0100

    ingress implementation

--- src/database.js
+++ src/database.js
@@ -5,7 +5,7 @@ const pool = new Pool({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  port: process.env.DATABASE_PORT,
+  port: parseInt(process.env.DATABASE_PORT),
 });
 
 module.exports = pool;
